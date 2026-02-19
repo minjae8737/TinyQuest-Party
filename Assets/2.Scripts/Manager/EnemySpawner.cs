e80e4299e1b093b574e1de1893eb3e05df328474
aljae <minjae8737@gmail.com>
2026-02-19 18:26:31 +0900

    Fix: 추가 유닛 데이터 조정

--- Assets/2.Scripts/Manager/EnemySpawner.cs
+++ Assets/2.Scripts/Manager/EnemySpawner.cs
@@ -4,7 +4,8 @@ using Random = UnityEngine.Random;
 public class EnemySpawner : MonoBehaviour
 {
     public Transform playerTr;
-
+    public UnitName spawnUnitName;
+    
     private void Start()
     {
         TestSpawn();
@@ -20,6 +21,6 @@ public class EnemySpawner : MonoBehaviour
         Vector2 randPos = new Vector2(Random.Range(4.5f, 5.5f) * (Random.value < 0.5f ? 1 : -1),
             Random.Range(9f, 11f) * (Random.value < 0.5f ? 1 : -1));
         // Debug.Log(randPos);
-        UnitManager.Instance.Spawn(UnitName.Armored_Orc, (Vector2)playerTr.position + randPos);
+        UnitManager.Instance.Spawn(spawnUnitName, (Vector2)playerTr.position + randPos);
     }
 }
